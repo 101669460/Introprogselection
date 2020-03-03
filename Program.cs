55e2c71fdb1e36fa9660c53cad596af6f649c953
DYLAN DINSDALE <101669460@student.swin.edu.au>
2020-03-03 14:07:44 +1100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Introprogselection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            age = 18;
+            int legal_age;
+            legal_age = 18;
+            if (age >= legal_age)
+            {
+                Console.WriteLine("Drink");
+            }
+            else
+            {
+                Console.Writeline("Don't Drink")
+                
+            }
+        }
+    }
+}
+
