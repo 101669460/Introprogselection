c04107048d997bb0f4bd7fd5a85026c61568116f
DYLAN DINSDALE <101669460@student.swin.edu.au>
2020-03-03 16:42:08 +1100

    almost finished guessing game

--- Program.cs
+++ Program.cs
@@ -12,9 +12,8 @@ namespace Introprogselection
             int a = rnd.Next(0, 25);
             Console.WriteLine("Guess the number between 0 and 25");
             int test = 0;
-           
+            Console.WriteLine("Enter Guess:");
             do{
-                Console.WriteLine("Enter Guess:");
                 int g1 = Convert.ToInt32(Console.ReadLine());
                 if (g1==a) 
                 {
@@ -30,11 +29,13 @@ namespace Introprogselection
                     {
                         Console.WriteLine("Nope, it's greater than that");
                     }
-                }
+                    Console.WriteLine("You have " + (4 - test) + " guesses left!");
+                } 
                 test++;
             } 
-            while(test<8);
-                if (test<8) 
+            while(test<5);
+                
+                if (test<5) 
                 {
                     Console.WriteLine("Damn. You Win!");
                     Console.WriteLine("It took you this many guesses:");
@@ -42,8 +43,8 @@ namespace Introprogselection
                 }
                 else
                 {
-                    Console.WriteLine("Nope, the answer was:");
-                    System.Console.WriteLine(a);
+                     Console.WriteLine("You have 0 guesses left!");
+                    Console.WriteLine("Nope, the answer was: " + a);
                 }
             
         }
