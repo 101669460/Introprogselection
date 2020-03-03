1923d0fb498686fa7e0d2c0086ae67f674244461
DYLAN DINSDALE <101669460@student.swin.edu.au>
2020-03-03 16:07:42 +1100

    almost complete guessing game

--- Program.cs
+++ Program.cs
@@ -3,23 +3,49 @@
 namespace Introprogselection
 {
     class Program
+
     {
         static void Main(string[] args)
         {
-            int age;
-            age = 18;
-            int legal_age;
-            legal_age = 18;
-            if (age >= legal_age)
-            {
-                Console.WriteLine("Drink");
-            }
-            else
-            {
-                Console.Writeline("Don't Drink")
-                
-            }
+            Console.WriteLine("Hey There! Let's play a little guessing game.");
+            Random rnd = new Random();
+            int a = rnd.Next(0, 25);
+            Console.WriteLine("Guess the number between 0 and 25");
+            int test = 0;
+           
+            do{
+                Console.WriteLine("Enter Guess:");
+                int g1 = Convert.ToInt32(Console.ReadLine());
+                if (g1==a) 
+                {
+                    break;
+                }
+                else 
+                {
+                    if(g1>a) 
+                    {
+                        Console.WriteLine("Nope, it's less than that");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nope, it's greater than that");
+                    }
+                }
+                test++;
+            } 
+            while(test<8);
+                if (test<8) 
+                {
+                    Console.WriteLine("Damn. You Win!");
+                    Console.WriteLine("It took you this many guesses:");
+                    System.Console.WriteLine(test);
+                }
+                else
+                {
+                    Console.WriteLine("Nope, the answer was:");
+                    System.Console.WriteLine(a);
+                }
+            
         }
     }
 }
-
